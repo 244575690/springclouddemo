3dc1706b8da06c6cea5e4eb8673f873509212f72
heqiang <244575690@qq.com>
2019-06-28 15:05:33 +0800

    修改了一个测试

--- EurekaSserver_7001/src/main/java/com/springcloud/controller/Eureka7001Controller.java
+++ EurekaSserver_7001/src/main/java/com/springcloud/controller/Eureka7001Controller.java
@@ -3,7 +3,8 @@ package com.springcloud.controller;
 /**
  * @author ：HQ
  * @date ：Created in 2019/6/28 15:00
- * @description：
+ * @description：这是一个测试的controller
  */
 public class Eureka7001Controller {
+	int i = 0
 }
